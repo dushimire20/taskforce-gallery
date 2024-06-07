d2ddf7393dbb0a57ab119fb609af1db0e7a20ab2
dushimire20 <odushimire922@daviscollege.com>
2024-06-07 23:02:44 +0200

    challenge 1

---
+++ Challenges/arrayMap.js
@@ -0,0 +1,24 @@
+function testArrayConteguous(arr, target) {
+    let i = 0;
+    let sum = 0;
+
+    for ( let count = 0; count < arr.length; count ++){
+        sum = sum + arr[count];
+
+        while (sum > target && i <= count){
+            sum = sum - arr[i];
+            i++;
+        }
+
+        if (sum === target) {
+            return true;
+        }
+
+
+    }
+    return false;
+}
+
+const arr = [4,2,7,1,9,5];
+const target = 10;
+console.log(testArrayConteguous(arr,target));
